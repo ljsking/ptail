0a4f2aad1ba5da9e881c592061726d7371381cb4
Jaesung Lee <ljsking@gmail.com>
2011-10-06 21:20:24 +0900

    Change format of validator's log.

--- py/validator.py
+++ py/validator.py
@@ -4,6 +4,9 @@ from datetime import datetime
 
 import logging
 
+logging.basicConfig(level=logging.DEBUG,
+	format='%(asctime)s %(levelname)s %(message)s')
+
 before = 0
 
 monitoring_start = datetime.now()
@@ -22,25 +25,27 @@ for line in sys.stdin:
 		read_count += 1
 
 		if before+1 != line_id:
-			print >>sys.stderr, before, line_id
+			logging.error("before: %d now: %d"%(before, line_id))
 
 		before = line_id
 		logged_time = datetime.strptime(time_token, '%Y-%m-%d %H:%M:%S')
 		logged_time.replace(microsecond=int(micro_secs))
-		delta = datetime.now() - datetime.strptime(time_token, '%Y-%m-%d %H:%M:%S')
-		summed_elapsed += delta.microseconds
-		if 1000000 < delta.microseconds:
-			print >>sys.stderr, "over 1 seconds", delta.microseconds
+		delta = datetime.now() - logged_time
+		summed_elapsed += delta.seconds
+		#if 2 < delta.seconds:
+			#logging.error("over 2 seconds %d"%delta.seconds)
 
 		if 10 < (datetime.now() - monitoring_start).seconds:
 			monitoring_start = datetime.now()
-			print >>sys.stderr, "total counts", read_count
-			print >>sys.stderr, "avg elapsed %f ms"%(summed_elapsed/read_count/1000.)
+			logging.debug("now processing %s"%logged_time)
+			logging.debug("total counts %d"%read_count)
+			logging.debug("avg elapsed %f sec"%(float(summed_elapsed)/read_count))
 			read_count = 0
 			summed_elapsed = 0
 				
 				#print >>sys.stderr, delta, worker, line_id
+	except ValueError:
+		logging.exception('parse error %s'%' '.join(tokens[0:2]))
 	except:
 		#pass
 		logging.exception('Raise exception')
-		print >>sys.stderr, "parse error"
