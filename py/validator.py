7ad350f851cce339d67accea927dd3b191efa411
Jaesung Lee <ljsking@gmail.com>
2011-10-07 17:33:02 +0900

    Can specify log filename by passing argument. Change log format like CSV.

--- py/validator.py
+++ py/validator.py
@@ -2,15 +2,25 @@
 import sys
 from datetime import datetime
 
+import argparse
 import logging
 
+parser = argparse.ArgumentParser(description='Validate ptail output.')
+parser.add_argument('--logfile', 
+                   default='validator.log',)
+args = parser.parse_args()
+
 logging.basicConfig(level=logging.DEBUG,
+	filename=args.logfile,
+	filemode='w',
 	format='%(asctime)s %(levelname)s %(message)s')
 
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+#logging.getLogger('').addHandler(console)
+
 before = 0
-
 monitoring_start = datetime.now()
-
 
 read_count = 0
 summed_elapsed = 0
@@ -19,17 +29,18 @@ for line in sys.stdin:
 	try:
 		line = line.strip()
 		tokens = line.split()
-		time_token, micro_secs = ' '.join(tokens[0:2]).split('.')
+		time_tokens = ' '.join(tokens[0:2]).split('.')
 		worker = tokens[2]
 		line_id = int(tokens[3])
 		read_count += 1
 
-		if before+1 != line_id:
-			logging.error("before: %d now: %d"%(before, line_id))
+		#if before+1 != line_id:
+			#logging.error("before: %d now: %d"%(before, line_id))
 
 		before = line_id
-		logged_time = datetime.strptime(time_token, '%Y-%m-%d %H:%M:%S')
-		logged_time.replace(microsecond=int(micro_secs))
+		logged_time = datetime.strptime(time_tokens[0], '%Y-%m-%d %H:%M:%S')
+		if len(time_tokens) > 1:
+			logged_time.replace(microsecond=int(time_tokens[1]))
 		delta = datetime.now() - logged_time
 		summed_elapsed += delta.seconds
 		#if 2 < delta.seconds:
@@ -38,8 +49,8 @@ for line in sys.stdin:
 		if 10 < (datetime.now() - monitoring_start).seconds:
 			monitoring_start = datetime.now()
 			logging.debug("now processing %s"%logged_time)
-			logging.debug("total counts %d"%read_count)
-			logging.debug("avg elapsed %f sec"%(float(summed_elapsed)/read_count))
+			logging.debug("monitoring-data(totalcntFor10secs,tps,elpasedtime) %d,%d,%f"
+				%(read_count, read_count/10, float(summed_elapsed)/read_count))
 			read_count = 0
 			summed_elapsed = 0
 				
